7f02f0e10515bbf21c91716fa3c681f02d3e5aab
张洋雷 <1258157385@qq.com>
2022-05-31 22:35:52 +0800

    chore: 编辑环境配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly'
+  },
   env: {
     browser: true,
     es2021: true
@@ -17,5 +23,10 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': ['error', {
+      ignores: ['index']
+    }]
   }
 }
